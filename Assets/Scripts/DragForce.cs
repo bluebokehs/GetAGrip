2d637c216a820c69e353d5fe032a130c928009d8
Owen Kern <owenkern@MacBook-Pro-3.local>
2020-05-16 17:37:49 -0400

    new beginning of climbing

---
+++ Assets/Scripts/DragForce.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragForce : MonoBehaviour
+{
+    public float power = 2f;
+    public Rigidbody2D rb;
+
+    public Vector2 minPower;
+    public Vector2 maxPower;
+
+    Camera camera;
+    Vector3 camOffset = new Vector3(0, 0, 15);
+    Vector2 force;
+
+    Vector3 startPos;
+    Vector3 endPos;
+
+    public TrajectoryLine tl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+        tl = GetComponent<TrajectoryLine>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check for a single touch 
+        if (Input.touchCount > 0)
+        {
+
+
+            Touch touch = Input.GetTouch(0);
+
+            // when the finger hits the screen
+            if (touch.phase == TouchPhase.Began)
+            {
+                startPos = camera.ScreenToWorldPoint(touch.position) + camOffset;
+            }
+            // when the finger drags across the screen
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector3 currentPos = camera.ScreenToWorldPoint(touch.position) + camOffset;
+                tl.RenderLine(startPos, currentPos);
+            }
+            // when the finger leaves the screen
+            if (touch.phase == TouchPhase.Ended)
+            {
+                endPos = camera.ScreenToWorldPoint(touch.position) + camOffset;
+
+                force = new Vector2(Mathf.Clamp(startPos.x - endPos.x, minPower.x, maxPower.x), Mathf.Clamp(startPos.y - endPos.y, minPower.y, maxPower.y));
+                rb.AddForce(force * power, ForceMode2D.Impulse);
+                tl.EndLine();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TrajectoryLine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class TrajectoryLine : MonoBehaviour
+{
+    public LineRenderer lr;
+    
+    void Awake()
+    {
+        lr = gameObject.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    public void RenderLine(Vector3 startPos, Vector3 endPos)
+    {
+        lr.positionCount = 2;
+        Vector3[] points = new Vector3[2];
+        points[0] = startPos;
+        points[1] = endPos;
+
+        lr.SetPositions(points);
+    }
+
+    public void EndLine()
+	{
+        lr.positionCount = 0;
+	}
+}
